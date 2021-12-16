77fda435d4a08de0ddfe80c4448f0d6018d3d652
Casey <cbradshaw.d13@gmail.com>
2021-12-16 09:58:08 -0500

    update Figure 6 divergence criterion

--- figs/Figure6.R
+++ figs/Figure6.R
@@ -10,25 +10,28 @@ diverged<-rep(NA,length(stepsizes))
 converged<-rep(NA,length(stepsizes))
 
 set.seed(85330)
+
 for(j in 1:10){
   niter<-rep(NA,ns)
   conv<-rep(NA,ns)
-  betamat<-matrix(nrow=ns,ncol=4)
+  eigs<-rep(NA,ns)
+  
   for(i in 1:ns){
     x=matrix(rnorm(n*4,mean=0,sd=2),nrow=n)
     x[,1]<-rep(1,n)
     y=as.vector(x%*%beta_gen+rnorm(n,mean=0,sd=s0))
-    out=NoisyNewton(x=x,y=y,private=T,mu=2,scale=T,stepsize=stepsizes[j],maxiter=20,suppress.inference=TRUE,stopping="non-private",mnorm=sqrt(2)) #vary the step size
-    betamat[i,]<-out$beta
+    out=NoisyNewton(x=x,y=y,private=T,mu=2,scale=T,stepsize=stepsizes[j],maxiter=20,suppress.inference=TRUE,stopping="private",mnorm=sqrt(2)) #vary the step size
     
     niter[i]<-out$iter
-    conv[i]=out$nonpriv_converge
+    conv[i]=out$priv_converge
+    eigs[i]<-out$eig_min
   }
   
-  diverged[j]<-sum(niter<20 & conv==0)/ns
+  diverged[j]<-mean(eigs<1e-14)
   converged[j]<-mean(conv)
 }
 
 
 plot(1:length(stepsizes),diverged,xlab="Step Size",ylab="% Trials Diverging",xaxt='n',pch=16, main=NULL)
 axis(1, at=1:10, labels=stepsizes)
+
